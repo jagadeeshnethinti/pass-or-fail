bd8113af8ee6d83a336ad9fa61913c69dd04203a
jagadeeshnethinti <109734275+jagadeeshnethinti@users.noreply.github.com>
2022-11-24 19:49:26 +0530

    Add files via upload

---
+++ pass pyt.py
@@ -0,0 +1,13 @@
+sno=int(input())
+name=str(input())
+group=str(input())
+sub1=int(input())
+sub2=int(input())
+sub3=int(input())
+print('sno:',sno)
+print('name:',name)
+print('group:',group)
+if sub1>=35 and sub2>=35 and sub3>=35 :
+    print('pass')
+else :
+    print('fail')
